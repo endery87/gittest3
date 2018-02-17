d695f087f6adf6d5e42a71d1ea9a02af9ff75097
samsungg <samsungg@samsung>
2018-02-17 18:58:37 +0200

    commit after checkin

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("git start");
 		System.out.println("first commit");
+		System.out.println("second commit");
 	}
 }
